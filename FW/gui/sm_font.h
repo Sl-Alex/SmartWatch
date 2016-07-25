b2d8331fe07a9d10674be34df3dde5123a532eab
Oleksii Slabchenko <alexslabchenko@gmail.com>
2016-07-26 00:35:59 +0300

    Final SmFont concept, ready to implement

--- FW/gui/sm_font.h
+++ FW/gui/sm_font.h
@@ -9,21 +9,37 @@
  * @par Font element
  *
  * Font element holds a group of images.
- * It has a header with symbols count, font height, baseline and an array of offsets for
- * each symbol. Each symbol has an image element layout.
+ * It has a header with symbols count, font height, baseline and an array of offsets and symbols
+ * in UCS-4 encoding. All symbol data are located then in the image element format.
+ *
+ * Symbols itself are not used directly by the firmware. Instead of this, firmware uses its own
+ * code table in order to minimize fonts and to make an indexed access to any symbol in the font
+ * instead of a search.
+ *
+ * For example symbol1 from the table below can be letter "A" or "z" or any other symbol. It does not
+ * really matter because all conversion is done on the smartphone side, firmware knows nothing about the symbols.
+ * It just receives something like symbol1 index and it knows exactly where it is stored.
+ * Internal strings used by the firmware are encoded in the same manner before compilation with
+ * an external tool(SmFlashPacker)
+ *
+ * @todo Write SmFlashPacker
+ *
  * So, font layout in the @ref SmHwStorage is the following:
  *
- * Offset  | Parameter | Size | Description
- * ------- | --------- | ---- | ------
- * 0x0000  | count     | 32   | Image width in pixels
- * 0x0004  | height    | 32   | Image height in pixels
- * 0x0008  | baseLine  | 32   | Image size in bytes
- * 0x000C  | offset1   | 32   | First symbol offset
- * 0x0010  | offset2   | 32   | Second symbol offset
- * ...     | ...       | 32   | ...
- * offset1 | symbol1   | ---  | Symbol1 image
- * offset2 | symbol2   | ---  | Symbol2 image
- * ...     | ...       | ...  | ...
+ * Offset   | Parameter | Size | Description
+ * -------- | --------- | ---- | ------
+ * 0x0000   | count     | 32   | Image width in pixels
+ * 0x0004   | height    | 32   | Image height in pixels
+ * 0x0008   | baseLine  | 32   | Image size in bytes
+ * 0x000C   | offset1   | 32   | First symbol offset
+ * 0x0010   | offset2   | 32   | Second symbol offset
+ * ...      | ...       | 32   | ...
+ * 0xNNNN   | symbol1   | ---  | Symbol1 (UCS-4)
+ * 0xNNNN+4 | symbol2   | ---  | Symbol2 (UCS-4)
+ * ...      | ...       | ...  | ...
+ * offset1  | image1    | ---  | Symbol1 image
+ * offset2  | image2    | ---  | Symbol2 image
+ * ...      | ...       | ...  | ...
  *
  * @}
  *
