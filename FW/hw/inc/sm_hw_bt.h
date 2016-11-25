59b3540ef43c537c8c8445e76998ddad7ec8d15a
Oleksii Slabchenko <alexslabchenko@gmail.com>
2016-11-25 16:32:43 +0200

    This closes #38 (BT communication)

--- FW/hw/inc/sm_hw_bt.h
+++ FW/hw/inc/sm_hw_bt.h
@@ -8,14 +8,100 @@
 #include "emulator_window.h"
 #endif
 
+/** @file
+ * 
+ * @page BLUETOOTH_COMMUNICATION Bluetooth communication
+ *
+ * @{
+ *
+ * @par Description
+ *
+ * Project uses HM-10 BLE module (BLE <==> UART bridge).
+ * Communication between the smartphone (or PC) and the SmartWatch is packet-based.
+ * Each request should be acknowledged, either with a data, or at least with empty acknowledge packet
+ * Packed is received by SmartWatch byte by byte. If time between two bytes is more than @ref SM_HW_BT_RX_TIMEOUT
+ * then previous bytes will be discarded.
+ *
+ * @par Packet structure
+ *
+ * @dot
+ * digraph packetstruct {node1 [label = "CRC32 (4 bytes) | Type (1 byte) | Content data (15 bytes)", shape = record];}
+ * @enddot
+ * 
+ * HM-10 transmits UART data in blocks, up to 20 bytes each, so 20 bytes was selected as a maximum
+ * packet size. Packet can be splitted in two parts: header and content.
+ * Header contains CRC32 and packet type, data varies depending on a packet type.
+ * Packet size is _always_ 20 bytes, unused bytes can contain random values.
+ * It is planned to use AES-256 encryption for all packets. Since block size for AES-256 is 128 bits, only last 16 bytes will be encrypted.
+ * Four CRC32 bytes remain unencrypted, so it is required to check other packet fields after decryption.
+ * Following packet types are implemented:
+ * 
+ * Type                                     | Description
+ * -----------------------------------------|------------
+ * @ref SM_HW_BT_PACKET_ACK                 | Acknowledge to any packet except of @ref SM_HW_BT_PACKET_VERSION
+ * @ref SM_HW_BT_PACKET_VERSION             | Version request and response
+ * @ref SM_HW_BT_PACKET_NOTIFICATION_HEADER | Text notification header, containing the length of the notification
+ * @ref SM_HW_BT_PACKET_NOTIFICATION_DATA   | Text notification data
+ * @ref SM_HW_BT_PACKET_UPDATE_HEADER       | Firmware update header, contains the size of the update
+ * @ref SM_HW_BT_PACKET_UPDATE_DATA         | Firmware update data.
+ * @ref SM_HW_BT_PACKET_DATETIME            | Set date/time request, contains both date and time
+ * 
+ * Now a bit more details about each type:
+ * 
+ * @par SM_HW_BT_PACKET_ACK
+ * 
+ * This is a response to any request packet except of @ref SM_HW_BT_PACKET_VERSION. It must contain a type
+ * and a sequence number of the request packet. If request packet does not contain a sequence number, then this field must be 0xFF
+ * 
+ * @par SM_HW_BT_PACKET_VERSION
+ * 
+ * This type of packet requests version information (ASCII, NULL-terminated) from the SmartWatch. Request data is not important. Response must have the same type.
+ * Response data is a NULL-terminated ASCII-encoded version string.
+ * 
+ * @par SM_HW_BT_PACKET_NOTIFICATION_HEADER
+ * 
+ * This is the notification request. It is followed by the variable number of the @ref SM_HW_BT_PACKET_NOTIFICATION_DATA packets. It contains notification size (bytes).
+ * 
+ * @par SM_HW_BT_PACKET_NOTIFICATION_DATA
+ * 
+ * This packet contains a portion of the notification data. It must have a sequence number, so the notification could be correctly assembled. Sequence number starts from zero
+ * and must be incremented with each new portion of data.
+ * 
+ * @par SM_HW_BT_PACKET_UPDATE_HEADER
+ * 
+ * This is the firmware update request. It is followed by the variable number of the @ref SM_HW_BT_PACKET_UPDATE_DATA packets. It contains firmware size (bytes).
+ * 
+ * @par SM_HW_BT_PACKET_UPDATE_DATA
+ * 
+ * This packet contains a portion of the new firmware update data. It must have a sequence number, so the firmware could be correctly assembled. Sequence number starts from zero
+ * and must be incremented with each new portion of data.
+ * 
+ * @par SM_HW_BT_PACKET_DATETIME
+ * 
+ * This packet synchronizes local time on a SmartWatch with the remote time (from Android or PC). It contains year, month, day, hours (in a 24-hour format), minutes and seconds.
+ * 
+ */
+ 
 /// @brief BLE packet size (must be less than 20)
 #define SM_HW_BT_PACKET_SIZE 20
 
-//extern SmHwBtPacket;
-struct SmHwBtPacket;
+/// @brief BLE receive timeout, milliseconds
+#define SM_HW_BT_RX_TIMEOUT  100
 
 #define SM_HW_BT_PACKET_HEADER_SIZE (5)
 #define SM_HW_BT_PACKET_CONTENT_SIZE (SM_HW_BT_PACKET_SIZE - SM_HW_BT_PACKET_HEADER_SIZE)
+
+/// @brief BLE packet definitions
+enum SmHwBtPacketType
+{
+    SM_HW_BT_PACKET_ACK,                 ///< Acknowledge to any packet except of SM_HW_BT_PACKET_VERSION
+    SM_HW_BT_PACKET_VERSION,             ///< Version request and response
+    SM_HW_BT_PACKET_NOTIFICATION_HEADER, ///< Text notification header, containing the length of the notification
+    SM_HW_BT_PACKET_NOTIFICATION_DATA,   ///< Text notification data
+    SM_HW_BT_PACKET_UPDATE_HEADER,       ///< Firmware update header, contains the size of the update
+    SM_HW_BT_PACKET_UPDATE_DATA,         ///< Firmware update data.
+    SM_HW_BT_PACKET_DATETIME             ///< Set date/time request, contains both date and time
+};
 
 struct __attribute__((__packed__)) SmHwBtPacket
 {
